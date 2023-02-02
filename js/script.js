79ed61e9ea076f1b2f4fd0cd5e110a876cc1ff7d
wildanwp24 <122135291+wildanwp24@users.noreply.github.com>
2023-02-02 09:12:08 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//toggle class active
+const navbarnav = document.querySelector(".navbar-nav");
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
